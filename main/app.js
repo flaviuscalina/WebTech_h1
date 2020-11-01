f3702670d0f27386d07e2277caa4a6dfc50490ee
Flavius-Cristian Calina <73759968+flaviuscalina@users.noreply.github.com>
2020-11-01 16:19:48 +0200

    Update app.js

--- main/app.js
+++ main/app.js
@@ -1,5 +1,26 @@
 function removeOrderItem(orderInfo, position){
-    
+    //1
+    if((orderInfo["items"] instanceof Array)==false){
+        throw "Items should be an array";
+    }
+  //3
+   if(position > orderInfo.items.length){
+        throw "Invalid position"
+    }
+  //2
+     for(let i = 0; i < orderInfo.items.length; i++){
+        if(!(orderInfo.items[i].hasOwnProperty("price") && 
+        orderInfo.items[i].hasOwnProperty("quantity"))){
+            throw "Malformed item";
+        }
+    }
+
+  //5
+    orderInfo.total = orderInfo.total - orderInfo.items[position].price * orderInfo.items[position].quantity;
+  
+  //4
+    orderInfo.items.splice(position, 1);
+    return orderInfo;
 }
 
 const app = {
